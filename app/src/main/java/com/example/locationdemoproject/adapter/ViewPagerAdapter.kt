5c1b2bc96b26682119bd5f0674a99aa8ad54031a
alamincmt <alamin.fits@gmail.com>
2022-12-19 14:56:20 +0600

    ViwePager2 Added with three tabs.

---
+++ app/src/main/java/com/example/locationdemoproject/adapter/ViewPagerAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.locationdemoproject.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+  
+    // declare arrayList to contain fragments and its title
+    private val mFragmentList = ArrayList<Fragment>()
+    private val mFragmentTitleList = ArrayList<String>()
+
+    fun addFragment(fragment: Fragment, title: String) {
+        // add each fragment and its title to the array list
+        mFragmentList.add(fragment)
+        mFragmentTitleList.add(title)
+    }
+
+    override fun getItemCount(): Int {
+        return mFragmentList.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return mFragmentList[position]
+    }
+}
