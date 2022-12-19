e1ebd36c37e88d6f2050fda0b3146039c64882e8
Md. Shanzid Hassan Jahangir <jahangiralom68@gmail.com>
2022-12-19 17:25:28 +0600

    device location permission added and location info shows

--- app/src/main/java/com/instantwebb/deviceinfo/MainActivity.kt
+++ app/src/main/java/com/instantwebb/deviceinfo/MainActivity.kt
@@ -1,31 +1,20 @@
 package com.instantwebb.deviceinfo
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.location.Address
-import android.location.Geocoder
-import android.location.Location
-import android.location.LocationManager
 import android.net.ConnectivityManager
 import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.app.ActivityCompat
 import androidx.viewpager2.widget.ViewPager2
 import com.instantwebb.deviceinfo.adapter.ViewPagerAdapter
 import com.instantwebb.deviceinfo.fragments.DeviceInfoFragment
 import com.instantwebb.deviceinfo.fragments.LocationFragment
 import com.instantwebb.deviceinfo.fragments.NetworkFragment
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import java.io.BufferedInputStream
@@ -40,9 +29,6 @@ import java.util.concurrent.FutureTask
 
 
 open class MainActivity : AppCompatActivity() {
-    private lateinit var mFusedLocationClient: FusedLocationProviderClient
-    private val permissionId = 2
-    var newAddress: String? = null
 
     private lateinit var viewPager2: ViewPager2
     private lateinit var tabLayout: TabLayout
@@ -51,11 +37,9 @@ open class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         initViews()
         initTabs()
-
-
         setSupportActionBar(toolbar)
     }
 
@@ -90,84 +74,6 @@ open class MainActivity : AppCompatActivity() {
         toolbar = findViewById(R.id.toolbar)
     }
 
-    @SuppressLint("MissingPermission", "SetTextI18n")
-    private fun getLocation() {
-        if (checkPermissions()) {
-            if (isLocationEnabled()) {
-                mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
-                    val location: Location? = task.result
-                    if (location != null) {
-                        val geocoder = Geocoder(this, Locale.getDefault())
-                        val list: List<Address>? =
-                            geocoder.getFromLocation(location.latitude, location.longitude, 1)
-                        val fullAddress = "Address: " + getFormattedAddress(list!!)
-                        /*tvAddress?.text = fullAddress
-                        tvLocation?.text = "Latitude: ${location.latitude}\nLongitude: ${location.longitude}," +
-                                "\nCountry Code: ${list[0].countryCode}"*/
-
-                    } else {
-//                        tvLocation?.text = "Location not found, Please try again."
-                    }
-                }
-            } else {
-                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
-                startActivity(intent)
-            }
-        } else {
-            requestPermissions()
-        }
-    }
-
-    private fun isLocationEnabled(): Boolean {
-        val locationManager: LocationManager =
-            getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
-            LocationManager.NETWORK_PROVIDER
-        )
-    }
-
-    private fun checkPermissions(): Boolean {
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED &&
-            ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            return true
-        }
-        return false
-    }
-
-    private fun requestPermissions() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ),
-            permissionId
-        )
-    }
-
-    @SuppressLint("MissingSuperCall")
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        if (requestCode == permissionId) {
-            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                getLocation()
-            }
-        }
-    }
-
-    fun getUserLocation(view: View) {
-        getLocation()
-    }
 
     @RequiresApi(Build.VERSION_CODES.M)
     fun getDeviceIP(view: View) {
@@ -207,56 +113,6 @@ open class MainActivity : AppCompatActivity() {
             ex.printStackTrace()
         }
         return null
-    }
-
-    private fun getFormattedAddress(addresses: List<Address>): String? {
-        try {
-            if (addresses[0].thoroughfare != null && addresses[0].thoroughfare == "Unnamed Road") {
-                val str1 = addresses[1].getAddressLine(0)
-                val str2 = addresses[2].getAddressLine(0)
-                if (str1 != null) {
-                    if (str1.contains("+") || str1.contains("Unnamed Road")) {
-                        newAddress = str2
-                    } else {
-                        newAddress = str1
-                    }
-
-                } else {
-                    if (str2.contains("+") || str2.contains("Unnamed Road")) {
-                        newAddress = str1
-                    } else {
-                        newAddress = str2
-                    }
-                }
-
-            } else {
-                val str = addresses[0].getAddressLine(0)
-                if (str.contains("+") || str.contains("Unnamed Road")) {
-                    val str1 = addresses[1].getAddressLine(0)
-                    val str2 = addresses[2].getAddressLine(0)
-                    if (str1 != null) {
-                        if (str1.contains("+") || str1.contains("Unnamed Road")) {
-                            newAddress = str2
-                        } else {
-                            newAddress = str1
-                        }
-
-                    } else {
-                        if (str2.contains("+") || str2.contains("Unnamed Road")) {
-                            newAddress = str1
-                        } else {
-                            newAddress = str2
-                        }
-                    }
-                } else {
-                    newAddress = str
-                }
-            }
-        } catch (e: java.lang.Exception) {
-            e.printStackTrace()
-        }
-
-        return newAddress
     }
 
     open fun getPublicIPAddress(): String? {
@@ -317,5 +173,17 @@ open class MainActivity : AppCompatActivity() {
     fun getDirection(view: View) {
         intent = Intent(this, MapsActivity::class.java)
         startActivity(intent)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (LocationFragment.getInstance() !=null){
+            LocationFragment.getInstance().getPermissionResult(grantResults,requestCode)
+        }
     }
 }
