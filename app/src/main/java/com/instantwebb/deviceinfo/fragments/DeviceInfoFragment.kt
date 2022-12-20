abfbd19f8ae4076e63aa4c2581192dd17884277d
Md. Shanzid Hassan Jahangir <jahangiralom68@gmail.com>
2022-12-20 15:35:03 +0600

    device advertisement id and uuid code

--- app/src/main/java/com/instantwebb/deviceinfo/fragments/DeviceInfoFragment.kt
+++ app/src/main/java/com/instantwebb/deviceinfo/fragments/DeviceInfoFragment.kt
@@ -1,12 +1,25 @@
 package com.instantwebb.deviceinfo.fragments
 
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.an.deviceinfo.device.model.Device
+import com.google.android.gms.ads.identifier.AdvertisingIdClient
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException
+import com.google.android.gms.common.GooglePlayServicesRepairableException
 import com.instantwebb.deviceinfo.databinding.FragmentDeviceBinding
+import kotlinx.coroutines.*
+import java.io.IOException
+import java.util.*
+
 
 class DeviceInfoFragment : Fragment() {
     private var _binding: FragmentDeviceBinding? = null
@@ -27,7 +40,10 @@ class DeviceInfoFragment : Fragment() {
         getDeviceInfo()
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    @SuppressLint("HardwareIds")
     private fun getDeviceInfo() {
+
         val device = Device(requireContext())
         binding.tvManufacturer.text = device.manufacturer
         binding.tvModel.text = device.model
@@ -43,7 +59,43 @@ class DeviceInfoFragment : Fragment() {
         binding.tvHeight.text = device.screenHeight.toString()
         binding.tvWidth.text = device.screenWidth.toString()
         binding.tvBuildBrand.text = device.buildBrand
-
+        getAdId()
+        binding.tvUuid.text = getUUID()
     }
 
+    @SuppressLint("HardwareIds")
+    fun getUUID(): String {
+        val androidId = Settings.Secure.getString(
+            requireContext().contentResolver,
+            Settings.Secure.ANDROID_ID
+        )
+        val androidId_UUID = UUID.nameUUIDFromBytes(androidId.toByteArray(charset("utf8")))
+        val unique_id = androidId_UUID.toString()
+        return unique_id
+    }
+
+    private fun getAdId(){
+        CoroutineScope(Dispatchers.IO).launch {
+
+            var idInfo: AdvertisingIdClient.Info? = null
+            try {
+                idInfo = AdvertisingIdClient.getAdvertisingIdInfo(requireContext())
+            } catch (e: GooglePlayServicesNotAvailableException) {
+                e.printStackTrace()
+            } catch (e: GooglePlayServicesRepairableException) {
+                e.printStackTrace()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            var advertId: String? = null
+            try {
+                advertId = idInfo!!.id
+                binding.tvDeviceAdId.text = advertId
+            } catch (e: NullPointerException) {
+                e.printStackTrace()
+            }
+            Log.d(TAG, "onCreate:AD ID $advertId")
+
+        }
+    }
 }
